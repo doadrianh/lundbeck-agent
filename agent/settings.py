d5e4ed755bf57c9332be811425270e7d6b3adc76
doadrianh <doadrianh@gmail.com>
2025-06-09 20:59:58 -0700

    fix: allow extra env variables

--- agent/settings.py
+++ agent/settings.py
@@ -3,17 +3,14 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     openai_api_key: str
-    langsmith_api_key: str
     pinecone_api_key: str
     tavily_api_key: str
     
-    langsmith_project: str
-    langsmith_endpoint: str = "https://api.smith.langchain.com"
-    langsmith_tracing: bool = True
     collection_name: str = "lundbeck-docs"
     chroma_persist_directory: str = ".chroma"
 
     class Config:
         env_file = ".env"
+        extra = "ignore"
 
 settings = Settings()
